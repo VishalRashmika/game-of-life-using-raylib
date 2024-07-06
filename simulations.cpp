9ea1e410b604e9fcecba6f0c9aa615b04a7119a7
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-06 08:11:03 +0530

    simulations source file

---
+++ simulations.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <utility>
+#include "simulation.hpp"
+
+void Simulation::Draw()
+{
+    grid.Draw();
+}
+
+void Simulation::SetCellValue(int row, int column, int value)
+{
+    grid.SetValue(row, column, value);
+}
